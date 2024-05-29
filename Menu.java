0ccd0247398947a25c12ecc807978b78b2ba3c41
Ryan Amorim <154550705+oRyan-Amorim@users.noreply.github.com>
2024-05-29 19:11:27 -0300

    Add files via upload

--- Menu.java
+++ Menu.java
@@ -13,11 +13,10 @@ public class Menu extends JFrame {
     private int xPosition;
     private static final int ANIMATION_SPEED = 10;
     private static final int TIMER_DELAY = 10;
-    private static final int BAR_WIDTH = 800;
-    private static final int BAR_WIDTH2 = 400;
+    private static final int BAR_WIDTH = 400;
     private JPanel greenBar;
-    // Adiciona uma referência para o segundo greenBar
     private JPanel secondGreenBar;
+    private JLabel logo;
 
     public Menu() {
         setTitle("ECOMAP");
@@ -31,17 +30,18 @@ public class Menu extends JFrame {
         // Painel 1
         panel1 = new JPanel();
         panel1.setLayout(null);
-        panel1.setBackground(new Color(0, 102, 51)); // Fundo bege
+        panel1.setBackground(new Color(0, 102, 51)); // Fundo verde
 
         titleLabel = new JLabel("BEM VINDO(A) AO ECOMAP");
         titleLabel.setFont(new Font("Helvetica", Font.BOLD, 18));
-        titleLabel.setForeground(new Color(246, 234, 215)); // Verde
-        titleLabel.setBounds(70, 200, 300, 50); // Ajuste as coordenadas conforme necessário
+        titleLabel.setForeground(new Color(246, 234, 215));
+        titleLabel.setBounds(70, 200, 300, 50);
         panel1.add(titleLabel);
+
         JLabel subtitleLabel = new JLabel("Reciclagem e gestão de resíduos");
         subtitleLabel.setFont(new Font("Helvetica", Font.PLAIN, 14));
         subtitleLabel.setForeground(new Color(246, 234, 215));
-        subtitleLabel.setBounds(84, 250, 300, 50); // Ajuste as coordenadas conforme necessário
+        subtitleLabel.setBounds(84, 250, 300, 50);
         panel1.add(subtitleLabel);
 
         nextButton = new JButton("<");
@@ -56,6 +56,7 @@ public class Menu extends JFrame {
             }
         });
         panel1.add(nextButton);
+
         closeButton = new JButton("X");
         closeButton.setFont(new Font("Helvetica", Font.BOLD, 20));
         closeButton.setForeground(new Color(0, 102, 51));
@@ -64,7 +65,8 @@ public class Menu extends JFrame {
         closeButton.setContentAreaFilled(false);
         closeButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                int escolha = JOptionPane.showConfirmDialog(Menu.this, "Tem certeza que deseja sair do ECOMAP?", "Sair", JOptionPane.YES_NO_OPTION);
+                int escolha = JOptionPane.showConfirmDialog(Menu.this, "Tem certeza que deseja sair do ECOMAP?", "Sair",
+                        JOptionPane.YES_NO_OPTION);
                 if (escolha == JOptionPane.YES_OPTION) {
                     System.exit(0);
                 }
@@ -73,14 +75,26 @@ public class Menu extends JFrame {
         panel1.add(closeButton);
 
         greenBar = new JPanel();
-        greenBar.setBackground(new Color(255, 255, 255)); // Verde
+        greenBar.setBackground(new Color(255, 255, 255));
+        greenBar.setLayout(null); // Permitir posicionamento absoluto
         greenBar.setBounds(400, 0, BAR_WIDTH, getHeight());
         panel1.add(greenBar);
+
+        // Adicionando a imagem ao painel 1
+        ImageIcon logoIcon = new ImageIcon("C:\\Users\\Ryan\\Downloads\\logo menu.png");
+        Image logoImage = logoIcon.getImage().getScaledInstance(200, 180, Image.SCALE_SMOOTH);
+        ImageIcon resizedLogoIcon = new ImageIcon(logoImage);
+        logo = new JLabel(resizedLogoIcon);
+        // Centralizando a logo na barra verde
+        int logoX = (BAR_WIDTH - 200) / 2;
+        int logoY = (getHeight() - 180) / 2;
+        logo.setBounds(logoX, logoY, 200, 180);
+        greenBar.add(logo);
 
         // Painel 2
         panel2 = new JPanel();
         panel2.setLayout(null);
-        panel2.setBackground(new Color(0, 102, 51)); // Bege
+        panel2.setBackground(new Color(0, 102, 51));
 
         closeButton = new JButton("X");
         closeButton.setFont(new Font("Helvetica", Font.BOLD, 20));
@@ -90,7 +104,8 @@ public class Menu extends JFrame {
         closeButton.setContentAreaFilled(false);
         closeButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                int escolha = JOptionPane.showConfirmDialog(Menu.this, "Tem certeza que deseja sair do ECOMAP?", "Sair", JOptionPane.YES_NO_OPTION);
+                int escolha = JOptionPane.showConfirmDialog(Menu.this, "Tem certeza que deseja sair do ECOMAP?", "Sair",
+                        JOptionPane.YES_NO_OPTION);
                 if (escolha == JOptionPane.YES_OPTION) {
                     System.exit(0);
                 }
@@ -100,23 +115,20 @@ public class Menu extends JFrame {
 
         JLabel loginText = new JLabel("Faça o login para continuar");
         loginText.setFont(new Font("Helvetica", Font.BOLD, 14));
-        loginText.setForeground(new Color(246, 234, 225)); // Verde
+        loginText.setForeground(new Color(246, 234, 225));
         loginText.setBounds(500, 150, 200, 50);
         panel2.add(loginText);
 
-        // Botão de login com fundo verde e texto bege
         JButton loginButton = new JButton("Login") {
             @Override
             protected void paintComponent(Graphics g) {
                 Graphics2D g2 = (Graphics2D) g.create();
                 g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-                // Desenha o fundo arredondado verde
-                g2.setColor(new Color(246, 234, 215)); // Verde
+                g2.setColor(new Color(246, 234, 215));
                 g2.fillRoundRect(0, 0, getWidth(), getHeight(), 15, 15);
 
-                // Desenha a borda arredondada
-                g2.setColor(new Color(246, 234, 215)); // Verde (mesma cor do fundo)
+                g2.setColor(new Color(246, 234, 215));
                 g2.setStroke(new BasicStroke(2));
                 g2.drawRoundRect(0, 0, getWidth(), getHeight(), 15, 15);
 
@@ -126,7 +138,7 @@ public class Menu extends JFrame {
         };
         loginButton.setFont(new Font("Helvetica", Font.BOLD, 14));
         loginButton.setBounds(515, 200, 150, 30);
-        loginButton.setForeground(new Color(0, 102, 51)); // Bege
+        loginButton.setForeground(new Color(0, 102, 51));
         loginButton.setContentAreaFilled(false);
         loginButton.setBorderPainted(false);
         loginButton.setFocusPainted(false);
@@ -140,7 +152,7 @@ public class Menu extends JFrame {
 
         JLabel resgitertxt = new JLabel("Se não tem Login, clique aqui para cadastrar-se!");
         resgitertxt.setFont(new Font("Helvetica", Font.BOLD, 14));
-        resgitertxt.setForeground(new Color(246, 236, 215)); // Verde
+        resgitertxt.setForeground(new Color(246, 236, 215));
         resgitertxt.setBounds(430, 250, 400, 50);
         panel2.add(resgitertxt);
 
@@ -148,12 +160,11 @@ public class Menu extends JFrame {
             @Override
             protected void paintComponent(Graphics g) {
                 Graphics2D g2 = (Graphics2D) g.create();
-                g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);                // Desenha o fundo arredondado verde
-                g2.setColor(new Color(246, 234, 215)); // Verde
+                g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                g2.setColor(new Color(246, 234, 215));
                 g2.fillRoundRect(0, 0, getWidth(), getHeight(), 15, 15);
 
-                // Desenha a borda arredondada
-                g2.setColor(new Color(246, 234, 215)); // Verde (mesma cor do fundo)
+                g2.setColor(new Color(246, 234, 215));
                 g2.setStroke(new BasicStroke(2));
                 g2.drawRoundRect(0, 0, getWidth(), getHeight(), 15, 15);
 
@@ -163,7 +174,7 @@ public class Menu extends JFrame {
         };
         registerButton.setFont(new Font("Helvetica", Font.BOLD, 14));
         registerButton.setBounds(465, 300, 250, 30);
-        registerButton.setForeground(new Color(0, 102, 51)); // Bege
+        registerButton.setForeground(new Color(0, 102, 51));
         registerButton.setContentAreaFilled(false);
         registerButton.setBorderPainted(false);
         registerButton.setFocusPainted(false);
@@ -177,7 +188,7 @@ public class Menu extends JFrame {
 
         backButton = new JButton(">");
         backButton.setFont(new Font("Helvetica", Font.BOLD, 20));
-        backButton.setForeground(new Color(0, 102, 51)); // Verde
+        backButton.setForeground(new Color(0, 102, 51));
         backButton.setBounds(10, 520, 50, 30);
         backButton.setBorderPainted(false);
         backButton.setContentAreaFilled(false);
@@ -188,7 +199,6 @@ public class Menu extends JFrame {
         });
         panel2.add(backButton);
 
-        // Adicionar os painéis ao CardLayout
         cards.add(panel1, "panel1");
         cards.add(panel2, "panel2");
 
@@ -208,15 +218,19 @@ public class Menu extends JFrame {
                 } else {
                     timer.stop();
                     cardLayout.show(cards, "panel2");
-                    // Remover o segundo greenBar se existir
                     if (secondGreenBar != null) {
                         panel2.remove(secondGreenBar);
-                        secondGreenBar = null; // Limpa a referência
+                        secondGreenBar = null;
                     }
                     secondGreenBar = new JPanel();
-                    secondGreenBar.setBackground(new Color(255, 255, 255)); // Verde
-                    secondGreenBar.setBounds(0, 0, BAR_WIDTH2, getHeight());
+                    secondGreenBar.setBackground(new Color(255, 255, 255));
+                    secondGreenBar.setLayout(null); // Permitir posicionamento absoluto
+                    secondGreenBar.setBounds(0, 0, BAR_WIDTH, getHeight());
                     panel2.add(secondGreenBar);
+                    int logoX = (BAR_WIDTH - 200) / 2;
+                    int logoY = (getHeight() - 180) / 2;
+                    logo.setBounds(logoX, logoY, 200, 180); // Centralizar a logo na barra branca
+                    secondGreenBar.add(logo);
                     panel2.revalidate();
                     panel2.repaint();
                 }
@@ -232,26 +246,29 @@ public class Menu extends JFrame {
                 if (xPosition > 0) {
                     panel1.setBounds(-(getWidth() - xPosition), 0, getWidth(), getHeight());
                     panel2.setBounds(xPosition - getWidth(), 0, getWidth(), getHeight());
-                    greenBar.setBounds(xPosition - BAR_WIDTH, 0, BAR_WIDTH, getHeight()); // Move o greenBar para a direita
+                    greenBar.setBounds(xPosition - BAR_WIDTH, 0, BAR_WIDTH, getHeight());
                     xPosition -= ANIMATION_SPEED;
                 } else {
                     timer.stop();
                     cardLayout.show(cards, "panel1");
-                    // Ajuste para restaurar o estado original do painel1
                     greenBar.setBounds(400, 0, BAR_WIDTH, getHeight());
-                    // Remove o segundo greenBar se existir
                     if (secondGreenBar != null) {
                         panel2.remove(secondGreenBar);
-                        secondGreenBar = null; // Limpa a referência
+                        secondGreenBar = null;
                         panel2.revalidate();
                         panel2.repaint();
                     }
+                    int logoX = (BAR_WIDTH - 200) / 2;
+                    int logoY = (getHeight() - 180) / 2;
+                    logo.setBounds(logoX, logoY, 200, 180); // Centralizar a logo na barra branca
+                    greenBar.add(logo);
+                    panel1.revalidate();
+                    panel1.repaint();
                 }
             }
         });
         timer.start();
     }
-    
 
     public static void main(String[] args) {
         new Menu();
