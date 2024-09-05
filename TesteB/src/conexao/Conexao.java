ff73cd13d7db235ac02d45a45dadf52c684cde8b
leosmar1609 <145773764+leosmar1609@users.noreply.github.com>
2024-09-05 12:39:49 -0300

    Update Conexao.java

--- TesteB/src/conexao/Conexao.java
+++ TesteB/src/conexao/Conexao.java
@@ -8,7 +8,7 @@ public class Conexao {
     private static Connection connection;
     private static final String URL = "jdbc:mysql://localhost:3306/ecobanco";
     private static final String USER = "root";
-    private static final String PASSWORD = "75869470Leo.";
+    private static final String PASSWORD = "root";
 
     private Conexao() { }
 
